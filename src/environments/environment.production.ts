ba45cf22d7291d50df64c9bd9407d0a147342b4a
Nannj446 <vallejosgabriel446@gmail.com>
2026-04-12 23:38:54 -0400

    fix: add apiUrl to production environment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://1p-si2-backend-production.up.railway.app/api/v1',
+  apiUrl: 'https://1p-si2-backend-production.up.railway.app/api/v1',
 };
